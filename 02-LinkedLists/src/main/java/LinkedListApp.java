a634aff91fdaa59c63663b04b290ba62ac0bf217
saityagci <saityagci@gmail.com>
2023-02-27 03:33:32 -0500

    LinkedListApp

--- 02-LinkedLists/src/main/java/LinkedListApp.java
+++ 02-LinkedLists/src/main/java/LinkedListApp.java
@@ -3,5 +3,12 @@ public class LinkedListApp {
      MySinglyLinkedList list1=new MySinglyLinkedList();
         for (int i = 0; i <5 ; i++) list1.add(i);
         list1.printNodes();
+        System.out.println(list1.indexOf(4));
+        System.out.println(list1.size);
+        list1.deleteNode(4);
+        System.out.println("After deleting "+5);
+        list1.printNodes();
+
+
     }
 }
